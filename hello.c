9b570b36344fc8ba5ade90dd9c83e81d03f24b39
PeterW-XD <peiran_wang@outlook.com>
2024-03-21 18:27:43 -0400

    Thu2

--- hello.c
+++ hello.c
@@ -92,15 +92,15 @@ int main()
 	point.x += point.dx;
 	point.y += point.dy;
 
-	if (point.x <= 0 || point.x >= WIDTH) {
+	if (point.x == 0 || point.x == WIDTH) {
 		point.dx = -point.dx;
 	}
-	if (point.y <= 0 || point.y >= HEIGHT) {
+	if (point.y == 0 || point.y == HEIGHT) {
 		point.dy = -point.dy;
 	}
 	input[0] = point.x % 256;
 	input[1] = point.y % 256;
-	input[2] = point.x / 256 * 2 + point.y / 256;
+	input[2] = point.x / 256 * 4 + point.y / 256;
 	vga_ball_color_t position = {input[0], input[1], input[2]};
 	set_background_color(&position);
     usleep(40000);
