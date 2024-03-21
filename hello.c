c6caedee8ecbdc91bd6ee4e0831c64bff613ee7e
PeterW-XD <peiran_wang@outlook.com>
2024-03-20 21:33:21 -0400

    test17

--- hello.c
+++ hello.c
@@ -77,8 +77,6 @@ int main()
     return -1;
   }
 
-  printf("initial state: ");
-  print_background_color();
 /*
   for (i = 0 ; i < 24 ; i++) {
     set_background_color(&colors[i % COLORS ]);
@@ -88,7 +86,7 @@ int main()
 */
 
 // Update ball position 
-	struct Point point = {10, 10, 1, 1};
+	struct Point point = {10, 10, 10, 10};
 	unsigned char input[3];
 	while (1) {
 	point.x += point.dx;
@@ -105,8 +103,7 @@ int main()
 	input[2] = point.x / 256 * 4 + point.y / 256; 
 	vga_ball_color_t position = {input[0], input[1], input[2]};
 	set_background_color(&position);
-  	print_background_color();
-    usleep(4000);
+    usleep(400000);
 	}
 
   printf("VGA BALL Userspace program terminating\n");
